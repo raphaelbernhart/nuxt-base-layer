967da5b35f4a268840aaaebf9273e4b7ddb6ae28
Raphael Bernhart <mail@raphaelbernhart.at>
2024-08-12 22:37:11 +0200

    🔧 Make tailwind config more default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,10 @@
-// import defaultTheme from 'tailwindcss/defaultTheme';
-
 export default {
     theme: {
-        // screens: {
-        // xs: '380px',
-        // ...defaultTheme.screens,
-        // },
         fontFamily: {
             sans: ['Source Sans Pro'],
         },
         extend: {
-            colors: {
-                primary: '#442615',
-                primaryLight: '#A18276',
-                secondary: '#233E3C',
-                secondaryLight: '#7A9E7E',
-                light: '#F3EED1',
-                background: '#271812',
-                backgroundLight: '#3D2920',
-            },
+            colors: {},
             fontSize: {
                 base: '0.9rem',
             },
